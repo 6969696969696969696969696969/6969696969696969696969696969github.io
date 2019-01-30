df9ba96daf784fc580cbef0aec45057ba6a86958
6969696969696969696969696969 <47137493+6969696969696969696969696969@users.noreply.github.com>
2019-01-30 17:41:38 +0800

    Add files via upload

---
+++ example.js
@@ -0,0 +1,14 @@
+alert("welcome")
+console.log("welcome");
+alert("請輸入年齡以確認是否符合資格前往網站");
+var A = prompt("年齡?");
+if (A >= 18){
+  console.log("A >= 18");
+    alert("還敢騙人阿 冰鳥")
+}
+else if (A < 18){
+  console.log("A < 18");
+    alert("死處男滾回家")
+}
+   console.log("fuck you");
+   alert("可撥單身狗")
